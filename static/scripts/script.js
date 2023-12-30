b070a93bdff0e60df295eb7d81f4e9bd872cbd56
Arthur L <18040415+alouradou@users.noreply.github.com>
2023-12-30 20:03:46 +0100

    Styling

---
+++ static/scripts/script.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const btnsWithLoader = document.querySelectorAll(".btn-with-loader");
+
+    btnsWithLoader.forEach(function (btn) {
+        const loaderId = btn.closest(".button-container").getAttribute("data-loader-id");
+        const loader = document.getElementById(loaderId);
+
+        btn.addEventListener("click", function () {
+            loader.style.display = "block";
+
+            // Ici, vous pouvez ajouter votre logique pour effectuer des requêtes et des calculs
+
+            // Simule une attente de 3 secondes (vous devrez remplacer cela par votre logique réelle)
+            setTimeout(function () {
+                loader.style.display = "none";
+                // Ajoutez ici le code pour rediriger vers la page spécifique
+                // window.location.href = btn.getAttribute("href");
+            }, 3000);
+        });
+    });
+});
