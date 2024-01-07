444b8afe9e64ac55499f41234c2bb4d221a8b675
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-01-07 16:45:22 +0100

    Added location and description (teacher)

--- src/calendar_manager.py
+++ src/calendar_manager.py
@@ -7,16 +7,18 @@ class CalendarManager:
         self.course_list = course_list
         self.cal = Calendar()  # Initialisez votre calendrier ici
 
-    def create_event(self, start_datetime, end_datetime, summary):
+    def create_event(self, start_datetime, end_datetime, summary, location, speaker):
         event = Event()
         event.add('summary', summary)
         event.add('dtstart', start_datetime)
         event.add('dtend', end_datetime)
+        event.add('location', location)
+        event.add('description', speaker)
         self.cal.add_component(event)
 
     def browse_course_list(self):
         for course in self.course_list:
-            self.create_event(course.start, course.end, course.description)
+            self.create_event(course.start, course.end, course.description, course.location, course.organizer)
 
     def save_calendar(self, file_path):
         with open(file_path, 'wb') as f:
--- src/course.py
+++ src/course.py
@@ -1,7 +1,12 @@
+import re
+
+
 class Course:
     def __init__(self):
         self.cell_content = ""
         self.description = ""
+        self.location = ""
+        self.organizer = ""
         self.start = None
         self.end = None
 
@@ -9,4 +14,11 @@ class Course:
         return f"Course: {self.description} ({self.start} - {self.end})"
 
     def get_description(self):
-        self.description = str(self.cell_content).split("\n")[0]
+        cell_content_parts = str(self.cell_content).split("\n")
+        try:
+            self.description = cell_content_parts[0]
+            self.location = "Salle " + cell_content_parts[-1].strip()
+            match = re.search(r'\((.*?)\)', cell_content_parts[-2] if len(cell_content_parts) > 1 else "")
+            self.organizer = match.group(1) if match else None
+        except IndexError:
+            self.description = cell_content_parts[0]
