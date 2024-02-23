6e3553d8145b2be65be29b2a648bfd9f49894226
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-02-23 11:56:05 +0100

    Added regex for location parsing

--- src/course.py
+++ src/course.py
@@ -1,6 +1,17 @@
 import re
 
 
+def get_location_string(cell_string):
+    try:
+        return "Salle " + str(int(cell_string))
+    except ValueError:
+        match = re.match(r'([(]?salle\s*)?(\d+)[)]?', cell_string, re.IGNORECASE)
+    if match:
+        return "Salle " + match.group(2)
+    else:
+        return cell_string
+
+
 class Course:
     def __init__(self):
         self.cell_content = ""
@@ -17,7 +28,7 @@ class Course:
         cell_content_parts = str(self.cell_content).split("\n")
         try:
             self.description = cell_content_parts[0]
-            self.location = "Salle " + cell_content_parts[-1].strip()
+            self.location = get_location_string(cell_content_parts[-1])
             match = re.search(r'\((.*?)\)', cell_content_parts[-2] if len(cell_content_parts) > 1 else "")
             self.organizer = match.group(1) if match else None
         except IndexError:
