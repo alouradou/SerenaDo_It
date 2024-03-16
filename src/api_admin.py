8f21c8e20942b0ef27169e888d83b5b238bb35d6
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-03-16 18:40:09 +0100

    Deleting courses from admin works

--- src/api_admin.py
+++ src/api_admin.py
@@ -1,3 +1,5 @@
+from flask import redirect
+
 from src.api_flask import *
 
 import sqlite3
@@ -31,3 +33,47 @@ def admin_view():
     print(categories)
     return render_template('admin.html',
                            categories=categories)
+
+
+@app.route("/admin/delete_course", methods=['POST'])
+def delete_course():
+    if request.method == 'POST':
+        course = request.form.get('course')
+        denomination = request.form.get('denomination')
+
+        conn = sqlite3.connect('./uploads/serenadoit.db')
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("DELETE FROM courses WHERE course_name = ? AND alias = ?", (course, denomination))
+            conn.commit()
+            print("Cours supprimé avec succès !")
+        except sqlite3.Error as e:
+            print("Erreur lors de la suppression du cours depuis la base de données :", e)
+        finally:
+            conn.close()
+
+        return redirect(url_for('admin_view'))
+
+
+@app.route("/admin/add_course", methods=['POST'])
+def add_course():
+    if request.method == 'POST':
+        course = request.form.get('course')
+        denomination = request.form.get('denomination')
+
+        conn = sqlite3.connect('./uploads/serenadoit.db')
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute("INSERT INTO courses (course_name, alias) VALUES (?, ?)", (course, denomination))
+            conn.commit()
+            print("Nouveau cours ajouté avec succès !")
+        except sqlite3.Error as e:
+            print("Erreur lors de l'ajout du nouveau cours dans la base de données :", e)
+        finally:
+            conn.close()
+
+        return redirect(url_for('admin_view'))
+
+
