87cd25d45160a641ec6afedc44f52d4ab1435e97
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-03-25 19:50:37 +0100

    Added caching to data_manager

--- src/data_manager.py
+++ src/data_manager.py
@@ -1,7 +1,11 @@
+import os
+
 import pandas as pd
 import openpyxl
 from openpyxl.cell import MergedCell
 import requests
+from sqlalchemy import text, create_engine
+from datetime import datetime, timedelta
 
 
 def parse_merged_cell(sheet, row, col):
@@ -26,6 +30,9 @@ def compute_timetable_header(df):
 
 class DataManager:
     def __init__(self, sheet_id, sheet_name="", saved_workbook_path="./tmp_workbook.xlsx"):
+        self.last_fetch = None
+        self.db_engine = create_engine("sqlite:///uploads/serenadoit.db")
+        self.sheet_id = sheet_id
         self.sheet_name = sheet_name
         self.saved_workbook_path = saved_workbook_path
         # https://stackoverflow.com/questions/33713084/download-link-for-google-spreadsheets-csv-export-with-multiple-sheets
@@ -33,8 +40,49 @@ class DataManager:
         self.xlsx_url = "https://docs.google.com/spreadsheets/d/" + sheet_id + "/export?format=xlsx&id=" + sheet_id
         self.workbook = self.load_workbook()
 
+    # Check if the df is in the db
+    def is_sheet_in_cache(self, delta=timedelta(hours=1)):
+        try:
+            with self.db_engine.connect() as conn:
+                fetch_dates = conn.execute(text("SELECT DISTINCT fetch_date "
+                                                "FROM fetch_dates "
+                                                "WHERE sheet_id = :sheet_id "
+                                                "ORDER BY fetch_date"),
+                                           {
+                                           "sheet_id": self.sheet_id,
+                                           "sheet_name": self.sheet_name
+                                           }).fetchall()
+                self.last_fetch = fetch_dates[-1][0]
+                self.last_fetch = datetime.strptime(self.last_fetch, "%Y-%m-%d %H:%M:%S.%f")
+                return self.last_fetch > datetime.now() - delta
+        except Exception as e:
+            print(f"Error checking cache: {e}")
+            return False
+
     def load_workbook(self):
+        if self.is_sheet_in_cache():
+            print("Loading workbook from cache")
+            return self.load_workbook_from_file()
+        else:
+            print("Loading workbook from internet")
+            return self.load_workbook_from_internet()
+
+    def load_workbook_from_file(self):
+        try:
+            return openpyxl.load_workbook(self.saved_workbook_path, data_only=True)
+        except Exception as e:
+            raise Exception(f"Error loading workbook from {self.saved_workbook_path}: {e}")
+
+    def load_workbook_from_internet(self):
         file = requests.get(self.xlsx_url, allow_redirects=True)
+        # Save date to database table "fetch_dates"
+        try:
+            df = pd.DataFrame({"fetch_date": [datetime.now()],
+                               "sheet_id": [self.sheet_id],
+                               "sheet_name": [self.sheet_name]})
+            df.to_sql("fetch_dates", self.db_engine, if_exists="append", index=False)
+        except Exception as e:
+            print(f"Error saving fetch date to database: {e}")
         with open(self.saved_workbook_path, 'wb') as f:
             try:
                 f.write(file.content)
